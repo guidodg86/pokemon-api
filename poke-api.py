cb8312707bdf9a48e9bf6b2a89f435c3a46a77d3
Guido de Giovanni <guidito.dg86@gmail.com>
2021-08-23 01:46:04 -0300

    / endpoint ready

--- poke-api.py
+++ poke-api.py
@@ -3,7 +3,7 @@ from flask_restful import request ,Resource, Api
 import requests
 
 POKE_API_URI_LIST = 'https://pokeapi.co/api/v2/pokemon/'
-SEARCH_LIMIT = 350
+SEARCH_LIMIT = 1000
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,24 +12,23 @@ api = Api(app)
 class PokeList(Resource):
     def get(self):
         query_params = {}
-        query_params['offset'] = int(request.args.get('offset'))
-        query_params['limit'] = int(request.args.get('limit'))
-        name_to_search = request.args.get('q')
+        try:
+            query_params['offset'] = int(request.args.get('offset'))
+        except TypeError:
+            query_params['offset'] = 0
+        except ValueError:
+            return {"error": "parameter offset must be a positive integer" }      
+        try:
+            query_params['limit']= int(request.args.get('limit'))
+        except TypeError:
+            query_params['limit'] = 10
+        except ValueError:
+            return {"error": "parameter limit must be a positive integer" }   
+        query_params['name_to_search'] = request.args.get('q')
 
 
-        if name_to_search == None:
-            if len(query_params):
-                target_uri = POKE_API_URI_LIST[:-1] + "?"
-                first = True
-                for param in query_params:
-                    if first:
-                        first = False
-                        target_uri = target_uri + param + "=" + str(query_params[param])
-                    else:
-                        target_uri = target_uri + "&" + param + "=" +  str(query_params[param])                 
-            else:
-                target_uri = POKE_API_URI_LIST
-
+        if query_params['name_to_search'] == None:
+            target_uri = POKE_API_URI_LIST[:-1] + "?offset=" + str(query_params['offset']) + "&limit=" + str(query_params['limit'])
             poke_response = requests.get(target_uri).json()
             poke_list_result = []
             poke_list_received = poke_response['results']
@@ -51,14 +50,13 @@ class PokeList(Resource):
             return final_response
         
         else:
-            print(POKE_API_URI_LIST[:-1] + "?limit=" + str(SEARCH_LIMIT) )
             poke_response = requests.get(POKE_API_URI_LIST[:-1] + "?limit=" + str(SEARCH_LIMIT) ).json()
             pokes_found =0
             poke_list_result = []
             while pokes_found<query_params['limit']:
                 poke_list_received = poke_response['results']
                 for pokemon in poke_list_received:
-                    if name_to_search in  pokemon['name']:
+                    if query_params['name_to_search'] in  pokemon['name']:
                         pokes_found += 1
                         pokemon_url = pokemon['url']
                         pokemon_id = pokemon_url.split("pokemon/")[1][:-1]
@@ -73,10 +71,12 @@ class PokeList(Resource):
                     break
                 poke_response = requests.get(poke_response['next']).json()
             
+            first_req_element = query_params['offset']
+            last_req_element = query_params['offset'] + query_params['limit'] 
             final_response = dict(  total=poke_response['count'],
                                     limit=query_params['limit'],
                                     offset=query_params['offset'],
-                                    data =  poke_list_result )
+                                    data =  poke_list_result[first_req_element:last_req_element] )
             return final_response
         
 
