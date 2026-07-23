b4c8bb8b029a436a836a6675de3f540a40939fee
Guido de Giovanni <guidito.dg86@gmail.com>
2026-07-23 18:23:08 +0100

    Starting get poke by name

--- poke-api-go/poke-api.go
+++ poke-api-go/poke-api.go
@@ -1,5 +1,6 @@
 package main
 
+// Global imports
 import (
 	"encoding/json"
 	"fmt"
@@ -12,27 +13,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Global constants
 const pokeApiUrl = "https://pokeapi.co/api/v2/pokemon/"
 const pokeSpriteUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 
+// Structs to receive data from the pokeapi
 type pokeApiList struct {
 	Count    int
 	Next     string
 	Previous string
 	Results  []pokeData
 }
-
 type pokeData struct {
 	Name string
 	Url  string
 }
 
+// Structs to send data to clients
 type pokeResult struct {
 	Id    string `json:"id"`
 	Name  string `json:"name"`
 	Image string `json:"image"`
 }
-
 type fullPokeResult struct {
 	Total  int          `json:"total"`
 	Limit  int          `json:"limit"`
@@ -40,12 +42,15 @@ type fullPokeResult struct {
 	Data   []pokeResult `json:"data"`
 }
 
+// Main with the endpoint declarations
 func main() {
 	router := gin.Default()
 	router.GET("/", getPokes)
+	router.GET("/:name", getPokeByName)
 	router.Run("localhost:8080")
 }
 
+// Home endpoint
 func getPokes(c *gin.Context) {
 	var listFinished bool = false
 	var targetUrl string = pokeApiUrl
@@ -54,10 +59,12 @@ func getPokes(c *gin.Context) {
 	var pokeDraft pokeResult
 	var apiResponse pokeApiList
 
+	// Parsing query parameters
 	limitStr := c.DefaultQuery("limit", "20")
 	offsetStr := c.DefaultQuery("offset", "0")
 	qName := c.DefaultQuery("q", "")
 
+	// Converting limit and offset to integers
 	limitInt, e1 := strconv.Atoi(limitStr)
 	if e1 != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{
@@ -71,11 +78,16 @@ func getPokes(c *gin.Context) {
 		return
 	}
 
+	// Count and limit for the response are already known
 	result.Limit = limitInt
 	result.Offset = offsetInt
+
+	// Setting up target Url for fetching the list of pokes
 	targetUrl = targetUrl + "?offset=" + offsetStr
 
+	// Main loop to fetch the data
 	for !listFinished {
+		// Perform the http request and parse it
 		fmt.Printf("poke-server: fetching from %s\n", targetUrl)
 		res, err := http.Get(targetUrl)
 		if err != nil {
@@ -95,6 +107,7 @@ func getPokes(c *gin.Context) {
 			os.Exit(1)
 		}
 
+		//Loop over the list of pokes we received filtering if needed
 		for _, v := range apiResponse.Results {
 			vSplit := strings.Split(v.Url, "/")
 			id := vSplit[len(vSplit)-2]
@@ -113,14 +126,20 @@ func getPokes(c *gin.Context) {
 			}
 		}
 
+		// Check if ending condition happened (no more pokes or reached limit)
 		if apiResponse.Next == "" || countPokes == limitInt {
 			listFinished = true
 		} else {
 			targetUrl = apiResponse.Next
 		}
 	}
+
+	// Sending the response
 	result.Total = apiResponse.Count
 	fmt.Printf("poke-server: full pokemon list fetched from server\n")
 	c.IndentedJSON(http.StatusOK, result)
+}
+
+func getPokeByName(c *gin.Context) {
 
 }
